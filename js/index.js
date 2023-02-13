05d3075e40211297829a38f043d2c01cb338af7c
lucyruan0419 <lucyruan0419@gmail.com>
2023-02-13 19:58:09 +0800

    index set cookie

--- js/index.js
+++ js/index.js
@@ -1,3 +1,36 @@
+// 設定 cookie
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+
+  // 取得 cookie 的值
+  function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+    
+  // 檢查某 cookie 是否存在
+  function checkCookie(cname) {
+    var cookie_value = getCookie(cname);
+    if (cookie_value != "") {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
 var swiper = new Swiper(".indexSwiper", {
     speed: 1500,
